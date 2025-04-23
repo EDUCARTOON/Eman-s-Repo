e8d7fe7a15c2d40933b0ccd7ee12e15b118fb45a
Lahlabooo <ellahloby22@gmail.com>
2025-04-23 02:33:07 +0200

    abdos commit

--- lib/features/mentors/data/models/mentors_model.dart
+++ lib/features/mentors/data/models/mentors_model.dart
@@ -1,16 +1,19 @@
 class MentorsModel {
   final String name;
   final String img;
+  final String cat;
 
   MentorsModel({
     required this.name,
     required this.img,
+    required this.cat,
   });
 
   factory MentorsModel.fromMap(Map<dynamic, dynamic> map) {
     return MentorsModel(
       name: map['name'] ?? '',
       img: map['imgUrl'] ?? '',
+      cat:map['cat'] ?? ''
     );
   }
 }
